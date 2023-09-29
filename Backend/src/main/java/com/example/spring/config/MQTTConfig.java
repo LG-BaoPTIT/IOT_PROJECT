0fef3a67bac6bf12760a581e83021009700b1eab
LG-BaoPTIT <bao2002pytn@gmail.com>
2023-09-29 21:02:18 +0700

    devB

--- Backend/src/main/java/com/example/spring/config/MQTTConfig.java
+++ Backend/src/main/java/com/example/spring/config/MQTTConfig.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 public class MQTTConfig {
     String clientEndpoint = "a1lwysb0m3hyfr-ats.iot.us-east-2.amazonaws.com";   // use value returned by describe-endpoint --endpoint-type "iot:Data-ATS"
     String clientId = "ModuleESP8266";                              // replace with your own client ID. Use unique client IDs for concurrent connections.
-    String awsAccessKeyId ="AKIA5NNN34MRMQ4SESOT";
-    String awsSecretAccessKey ="KdqmqaP0wcl+1rKVQi0ulDgc9SwsB+7ugKiI7Nm9";
+    String awsAccessKeyId ="";
+    String awsSecretAccessKey ="";
     String sessionToken =null;
 
 
